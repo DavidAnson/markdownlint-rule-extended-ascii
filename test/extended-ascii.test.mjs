4b39209dbb1e9c778ae6351a0c629c3b75b42810
David Anson <david@dlaa.me>
2024-05-09 21:54:22 -0700

    Add tests for JSON/YAML parameter configuration syntax.

--- test/extended-ascii.test.mjs
+++ test/extended-ascii.test.mjs
@@ -3,6 +3,11 @@
 import assert from "node:assert";
 import test from "node:test";
 import { main as cli2 } from "markdownlint-cli2";
+import markdownlint from "markdownlint-cli2/markdownlint";
+const { promises } = markdownlint;
+const { readConfig } = promises;
+import jsoncParse from "markdownlint-cli2/parsers/jsonc";
+import yamlParse from "markdownlint-cli2/parsers/yaml";
 
 const asciiViolations = [
 	"test/extended-ascii-violations.md:5:1 extended-ascii Only extended ASCII characters are allowed [Blocked character: 'Ä']",
@@ -27,17 +32,26 @@ test("extended ascii violations", async (t) => {
 	assert.deepEqual(messages, extendedAsciiViolations);
 });
 
-test("ascii-only violations", async (t) => {
+test("ascii-only violations, JSON configuration", async (t) => {
 	const messages = [];
 	const params = {
 		"argv": [ "test/extended-ascii-violations.md" ],
 		"logError": (message) => messages.push(message),
 		"optionsOverride": {
-			"config": {
-				"extended-ascii": {
-					"ascii-only": true
-				}
-			},
+			"config": await readConfig("./test/config.json", [ jsoncParse ])
+		}
+	}
+	assert.equal(await cli2(params), 1);
+	assert.deepEqual(messages, [ ...asciiViolations, ...extendedAsciiViolations ]);
+});
+
+test("ascii-only violations, YAML configuration", async (t) => {
+	const messages = [];
+	const params = {
+		"argv": [ "test/extended-ascii-violations.md" ],
+		"logError": (message) => messages.push(message),
+		"optionsOverride": {
+			"config": await readConfig("./test/config.yaml", [ yamlParse ])
 		}
 	}
 	assert.equal(await cli2(params), 1);
